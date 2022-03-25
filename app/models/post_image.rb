c76664b934232b34c1c77694f0a8b9957f576852
EC2 Default User <ec2-user@ip-172-31-83-232.ec2.internal>
2022-03-25 15:25:20 +0000

    ページネーション追加　5thcommit

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,7 +3,10 @@ class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
+  validates :shop_name, presence: true
+  validates :image, presence: true
+
   def get_image
     if image.attached?
       image
@@ -11,7 +14,7 @@ class PostImage < ApplicationRecord
       'no_image.jpg'
     end
   end
-  
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -19,9 +22,9 @@ class PostImage < ApplicationRecord
     end
     image
   end
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
 end
